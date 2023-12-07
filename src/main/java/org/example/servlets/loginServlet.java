fc466833c79edc9b1bf9a739e5d483e6a12254d5
Cuma <cumaerarslan2@gmail.com>
2023-12-07 20:04:29 +0100

    PEm datei ssl

--- src/main/java/org/example/servlets/loginServlet.java
+++ src/main/java/org/example/servlets/loginServlet.java
@@ -13,17 +13,7 @@ public class loginServlet extends HttpServlet {
 
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, IOException {
-        String benutzername = request.getParameter("benutzername");
-        String passwort = request.getParameter("passwort");
 
-         /*Überprüfe die Anmeldeinformationen in der Datenbank (verwende Hibernate)
-        boolean authentifiziert = Hier Hibernate verwenden, um die Anmeldeinformationen zu überprüfen */
-
-        if ("testuser".equals(benutzername) && "1".equals(passwort)) {
-            // Weiterleitung zur erfolgreichen Anmeldung
-            response.sendRedirect("dashboard.jsp");
-        } //else {
-            // Fehlermeldung oder Weiterleitung zur Login-Seite
             response.sendRedirect("register.jsp");
         //}
     }
