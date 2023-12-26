8cc7815bcd003dba7e845e2ccd3602b7f9e83580
Emin770 <femin952@gmail.com>
2023-12-26 23:09:43 +0100

    Passwort überprüfung von sonderzeichen und passwort länge. Email überprüfung von @.

--- src/main/java/org/example/servlets/registerServlet.java
+++ src/main/java/org/example/servlets/registerServlet.java
@@ -27,23 +27,37 @@ public class registerServlet extends HttpServlet {
 
         Mitarbeiter mitarbeiter = register_service.getMitarbeiterByPersonalNummer(Integer.parseInt(personalNummer));
 
-        // Todo: Email ungültig prüfen: Muss @ enthalten und Prefix .com z.b./ Email bereits vergeben prüfen
+        // Todo: Email ungültig prüfen: Prefix .com z.b./ Email bereits vergeben prüfen
         // Todo: Passwort ungültig prüfen: mindestens 8 Zeichen, mindestens 1 Großbuchstabe, mindestens 1 Zahl, mindestens 1 Sonderzeichen
         // Todo: Eingabe in die Datenbank in Kleinbuchstaben umwandeln / Außer Passwort
         if (mitarbeiter != null) {
             if (mitarbeiter.getGeburtsdatum() != geburtsdatum) {
-                if (register_service.comparePassword(password, passwordrepeat)) {
-                    register_service.registerUser(email, password, mitarbeiter);
-                    response.sendRedirect("index.jsp");
-                    // Todo: Erfolgsmeldung
+                if (password.length() >= 8 && !password.matches("[^!§$%&/()?=]+")) {
+                    if (register_service.comparePassword(password, passwordrepeat)) {
+                        if (email != null && email.contains("@")) {
+                            register_service.registerUser(email, password, mitarbeiter);
+                            response.sendRedirect("index.jsp");
+                        } else {
+                            //response.getWriter().println("Ungültige E-Mail-Adresse");
+                            // Todo: Fehlermeldung: Ungültige E-Mail-Adresse
+                        }
+                        // Todo: Erfolgsmeldung
+                    } else {
+                        response.sendRedirect("register.jsp");
+                        // Todo:  Fehlermeldung: Passwörter stimmen nicht überein
+                    }
+
                 } else {
-                    response.sendRedirect("register.jsp");
-                    // Todo:  Fehlermeldung: Passwörter stimmen nicht überein
+                    // Todo:  Fehlermeldung: Passwort muss mindestens 8 Zeichen lang sein oder ein sonderzeichen beinhalten
+                    response.getWriter().println("Passwort muss mindestens 8 Zeichen lang sein oder ein sonderzeichen beinhalten");
                 }
+
             } else {
-                response.sendRedirect("register.jsp");
-              //Todo:  Fehlermeldung: Geburtsdatum stimmt nicht überein
-            }
+                    response.sendRedirect("register.jsp");
+                    //Todo:  Fehlermeldung: Geburtsdatum stimmt nicht überein
+                }
+
+
         } else {
             response.sendRedirect("register.jsp");
             // Todo:  Fehlermeldung: Mitarbeiter nicht gefunden
