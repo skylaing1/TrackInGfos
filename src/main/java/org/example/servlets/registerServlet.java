a61279c280f0bb5a005525fa0e24f69f62d53e04
Cuma <cumaerarslan2@gmail.com>
2023-12-27 17:02:52 +0100

    Bessere Formatierung

--- src/main/java/org/example/servlets/registerServlet.java
+++ src/main/java/org/example/servlets/registerServlet.java
@@ -29,34 +29,39 @@ public class registerServlet extends HttpServlet {
 
         // Todo: Email ungültig prüfen: Prefix .com z.b./ Email bereits vergeben prüfen
         // Todo: Passwort ungültig prüfen: mindestens 8 Zeichen, mindestens 1 Großbuchstabe, mindestens 1 Zahl, mindestens 1 Sonderzeichen
-        // Todo: Eingabe in die Datenbank in Kleinbuchstaben umwandeln / Außer Passwort
+
         if (mitarbeiter != null) {
+
             if (mitarbeiter.getGeburtsdatum() != geburtsdatum) {
+
                 if (password.length() >= 8 && !password.matches("[^!§$%&/()?=]+")) {
+
                     if (register_service.comparePassword(password, passwordrepeat)) {
+
                         if (email != null && email.contains("@")) {
+
                             register_service.registerUser(email, password, mitarbeiter);
                             response.sendRedirect("index.jsp");
+                            // Todo: Erfolgsmeldung
                         } else {
-                            //response.getWriter().println("Ungültige E-Mail-Adresse");
+                            response.sendRedirect("register.jsp");
                             // Todo: Fehlermeldung: Ungültige E-Mail-Adresse
                         }
-                        // Todo: Erfolgsmeldung
+
                     } else {
                         response.sendRedirect("register.jsp");
                         // Todo:  Fehlermeldung: Passwörter stimmen nicht überein
                     }
 
                 } else {
+                    response.sendRedirect("register.jsp");
                     // Todo:  Fehlermeldung: Passwort muss mindestens 8 Zeichen lang sein oder ein sonderzeichen beinhalten
-                    response.getWriter().println("Passwort muss mindestens 8 Zeichen lang sein oder ein sonderzeichen beinhalten");
                 }
 
             } else {
                     response.sendRedirect("register.jsp");
                     //Todo:  Fehlermeldung: Geburtsdatum stimmt nicht überein
                 }
-
 
         } else {
             response.sendRedirect("register.jsp");
