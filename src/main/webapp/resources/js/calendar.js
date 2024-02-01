dfc73f675d90b8285c3664e76d2a4dc41e84187d
Cuma <cumaerarslan2@gmail.com>
2024-02-01 17:26:37 +0100

    Bug fixing: Timezone Datum und Datum Logik

--- src/main/webapp/resources/js/calendar.js
+++ src/main/webapp/resources/js/calendar.js
@@ -1,26 +1,65 @@
-var myModal = new bootstrap.Modal(document.getElementById('event-modal'));
+const myModal = new bootstrap.Modal(document.getElementById('event-modal'));
+const startDateInput = document.querySelector('#event-modal input[name="input_datum_von"]');
+const endDateInput = document.querySelector('#event-modal input[name="input_datum_bis"]');
+const entryIdInput = document.querySelector('#event-modal input[name="entry-id"]');
+const startTimeInput = document.querySelector('#event-modal input[name="input_uhrzeit_von"]');
+const endTimeInput = document.querySelector('#event-modal input[name="input_uhrzeit_bis"]');
+const descriptionInput = document.querySelector('#event-modal textarea[name="input_notizen"]');
+const statusInput = document.querySelector('#event-modal select[name="input_status"]');
+
+
+// Event Listener damit das Datum nicht kleiner als das Startdatum sein kann
+startDateInput.addEventListener('change', function() {
+    endDateInput.min = startDateInput.value;
+});
+
+// Event Listener damit das Datum nicht größer als das Enddatum sein kann
+endDateInput.addEventListener('change', function() {
+    startDateInput.max = endDateInput.value;
+});
+
 
 
 function editEvent(entry) {
     console.log(entry);
 
 
+    // Setzen der Werte in das Modal aus Datenbank falls vorhanden
+    entryIdInput.value = (entry ? entry.id : '') ?? null;
+    startDateInput.value = (entry ? entry.startDate : '');
+    endDateInput.value = (entry ? entry.endDate : '');
+    statusInput.value = (entry ? entry.status : '');
+    startTimeInput.value = (entry ? entry.startTime : '');
+    endTimeInput.value = (entry ? entry.endTime : '');
+    descriptionInput.value = (entry ? entry.description : '') ?? null;
 
-    document.querySelector('#event-modal input[name="entry-id"]').value = (entry ? entry.id : '') ?? null;
-    document.querySelector('#event-modal input[name="input_datum_von"]').value = (entry ? entry.startDate : '');
-    document.querySelector('#event-modal input[name="input_datum_bis"]').value = (entry ? entry.endDate : '');
-    document.querySelector('#event-modal select[name="input_status"]').value = (entry ? entry.status : '');
-    document.querySelector('#event-modal input[name="input_uhrzeit_von"]').value = (entry ? entry.startTime : '');
-    document.querySelector('#event-modal input[name="input_uhrzeit_bis"]').value = (entry ? entry.endTime : '');
-    document.querySelector('#event-modal textarea[name="input_notizen"]').value = (entry ? entry.description : '') ?? null;
+
+    // setzen der Min und Max Werte für die Uhrzeit wenn nur ein Tag ausgewählt ist
+    if (startDateInput.value === endDateInput.value) {
+        console.log("gleich");
+        endTimeInput.min = startTimeInput.value;
+        startTimeInput.max = endTimeInput.value;
+    }
+
+    // setzen der Min und Max Werte für das Datum
+    endDateInput.min = startDateInput.value;
+    startDateInput.max = endDateInput.value;
+
+
+
+
+
 
     myModal.show();
 }
 
 function saveEvent() {
+
+
+
     var entry = {
 
-
+        //TODO: Entfernen wenn nicht mehr benötigt
         status: document.querySelector('#event-modal select[name="input_status"]').value,
         startDate: new Date(document.querySelector('#event-modal input[name="input_datum_von"]').value),
         endDate: new Date(document.querySelector('#event-modal input[name="input_datum_bis"]').value),
@@ -72,9 +111,15 @@ new Calendar('#calendar', {
         }
     ],
     selectRange: function (e) {
-        var startDate = e.startDate.toISOString().split('T')[0];
-        var endDate = e.endDate.toISOString().split('T')[0];
-        editEvent({startDate: startDate, endDate: endDate});
+        console.log(e.startDate);
+
+        // Extrahieren das Datum im ISO-Format ohne Zeitzone
+        var startDate = new Date(e.startDate.getTime() - e.startDate.getTimezoneOffset() * 60000).toISOString().split('T')[0];
+        var endDate = new Date(e.endDate.getTime() - e.endDate.getTimezoneOffset() * 60000).toISOString().split('T')[0];
+
+        console.log(startDate);
+
+        editEvent({ startDate: startDate, endDate: endDate });
     },
 })
 document.getElementById('form').addEventListener('submit', function (e) {
